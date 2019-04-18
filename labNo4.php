016fc5661fa175230cf6dd2aee1b118095b589f1
Joan A. Jagelski <J.JAGELSKI@labs.clark.edu>
2019-04-18 12:55:16 -0700

    class work

---
+++ labNo4.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php 
+    // Define these errors in an array
+	$upload_errors = array(
+                            UPLOAD_ERR_OK 				=> "No errors.",
+                            UPLOAD_ERR_INI_SIZE  		=> "Larger than upload_max_filesize.",
+                            UPLOAD_ERR_FORM_SIZE 		=> "Larger than form MAX_FILE_SIZE.",
+                            UPLOAD_ERR_PARTIAL 			=> "Partial upload.",
+                            UPLOAD_ERR_NO_FILE 			=> "No file.",
+                            UPLOAD_ERR_NO_TMP_DIR 		=> "No temporary directory.",
+                            UPLOAD_ERR_CANT_WRITE 		=> "Can't write to disk.",
+                            UPLOAD_ERR_EXTENSION 		=> "File upload stopped by extension.");
+
+    if($_SERVER['REQUEST_METHOD'] == "POST"){
+        // what file do we need to move?
+        $tmp_file = $_FILES['file_upload']['tmp_name'];
+        // set target file name
+        // basename gets just the file name
+        $target_file = basename($_FILES['file_upload']['name']);
+        // set upload folder name
+        $upload_dir = 'images';
+        // Now lets move the file
+        // move_uploaded_file returns false if something went wrong
+        if(move_uploaded_file($tmp_file, $upload_dir . "/" . $target_file)){
+            $message = "File uploaded successfully";
+        } else {
+            $error = $_FILES['file_upload']['error'];
+            $message = $upload_errors[$error];
+        } // end of if
+    } 
+    else{
+        if(isset($_GET['del'])){
+            unlink($upload_dir/$GET[del])
+        
+        $dir = "images";
+        if ($_SERVER['REQUEST_METHOD'] == "GET") {
+            if(is_dir($dir)){
+                $dir_array = scandir($dir);
+                foreach ($dir_array as $file) {
+                    // don't display the . and .. directories. Using the strpos() for this.
+                    if(strpos($file,'.') > 0){
+                        echo "filename: {$file}<br/>";
+                        echo '<img src="images/$imagName" alt="">'
+                        echo "<a href="?del=$filename">
+                    }
+                }
+            } // end of if
+        }//end if
+    }//end else                    
+    
+    //scan to read contents of filder
+    // use loop to display inages
+    // <a href="?del=$filename">
+
+
+
+    ?>
+    <?php if(!empty($message)) {echo "<p>{$message}</p>";} ?>
+	<form action="" method="post" enctype="multipart/form-data">
+		<input type="hidden" name="MAX_FILE_SIZE" value="100000000">
+		<input type="file" name="file_upload">
+		<input type="submit" name="submit" value="Upload">
+	</form>
+</body>
+</html>
